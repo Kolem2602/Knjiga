6ff9d349947273dd263d2fd9c9b3bf5c5d15a987
Kolem2602 <44872337+Kolem2602@users.noreply.github.com>
2018-11-09 00:04:17 +0100

    Add files via upload

---
+++ Knjiga.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Knjiga
+    {
+        private double cijena;
+        private string autor;
+        private string naslov;
+
+
+        public string DohvatiNaslov()
+        {
+            return naslov;
+        }
+        public void PostaviNaslov(string naslov)
+        {
+            this.naslov = naslov;
+        }
+
+        public double DohvatiCijenu()
+        {
+            return cijena;
+        }
+        public void PostaviCijenu(double cijena)
+        {
+            this.cijena = cijena;
+        }
+
+        public string DohvatiAutor ()
+        {
+            return autor;
+        }
+        public void PostaviAutora(string autor)
+        {
+            this.autor = autor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+namespace OsobaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knjiga k1 = new Knjiga();
+            k1.PostaviAutora("Chris Carter");
+            k1.PostaviNaslov("Ubojica Crucifix");
+            k1.PostaviCijenu(139.28);
+
+            List<Knjiga> Knjige= new List<Knjiga>();
+            Knjige.Add(k1);
+        }
+    }
+}
