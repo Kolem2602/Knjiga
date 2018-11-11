2f427642fbba3662f6ce40a4fe6523579fe81661
ema kolaric <emakolaric@icloud.com>
2018-11-11 18:53:59 +0100

    Određivanje najskuplje knjige

---
+++ ConsoleApp1/Knjiga.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Knjiga
+    {
+        private double cijena;
+        private string autor;
+        private string naslov;
+
+
+        public string DohvatiNaslov()
+        {
+            return naslov;
+        }
+        public void PostaviNaslov(string naslov)
+        {
+            this.naslov = naslov;
+        }
+
+        public double DohvatiCijenu()
+        {
+            return cijena;
+        }
+        public void PostaviCijenu(double cijena)
+        {
+            this.cijena = cijena;
+        }
+
+        public string DohvatiAutor ()
+        {
+            return autor;
+        }
+        public void PostaviAutora(string autor)
+        {
+            this.autor = autor;
+        }
+        //dohvaca najskuplju knjigu
+       public void ispis (List<Knjiga> knjige)
+        {
+           
+            foreach (Knjiga k1 in knjige)
+            {
+                Console.WriteLine("Naziv knjige: {0}, Autor : {1}, Cijena: {2}", k1.DohvatiNaslov(), k1.DohvatiAutor(), k1.DohvatiCijenu());
+            }
+         
+        }
+
+        public void Najskuplja (List<Knjiga> knjige)
+        {
+           int br = 0;
+            double cj = 0;
+            for (int x = 0; x < knjige.Count(); x++)
+            {
+                Knjiga k1 = knjige.ElementAt(x);
+                if (k1.DohvatiCijenu() > cj)
+                {
+                    cj = k1.DohvatiCijenu();
+                    br = x;
+                }
+            }
+
+            Console.WriteLine("Najskuplja knjiga je {0} i njena cijena je {1}", knjige.ElementAt(br).DohvatiNaslov(), knjige.ElementAt(br).DohvatiCijenu());
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+namespace OsobaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knjiga k1 = new Knjiga();
+            k1.PostaviAutora("Chris Carter");
+            k1.PostaviNaslov("Ubojica Crucifix");
+            k1.PostaviCijenu(139.28);
+
+            List<Knjiga> Knjige = new List<Knjiga>();
+            Knjige.Add(k1);
+
+            Knjiga k2 = new Knjiga();
+            k2.PostaviAutora("jfhfh");
+            k2.PostaviNaslov("To kill a mockingbird");
+            k2.PostaviCijenu(300.18);
+            Knjige.Add(k2);
+
+            Knjiga k3 = new Knjiga();
+            k3.PostaviAutora("jfhfh");
+            k3.PostaviNaslov("Harry Potter");
+            k3.PostaviCijenu(186.18);
+            Knjige.Add(k3);
+
+            k1.ispis(Knjige);
+            k1.Najskuplja(Knjige);
+
+
+
+
+
+        }
+    }
+}
